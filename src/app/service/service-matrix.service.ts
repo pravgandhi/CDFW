1042b33c3307dba4316f2c64b97831af60050006
pravgandhi <pravgandhi@deloitte.com>
2019-03-22 14:07:45 -0400

    adding production api url

--- src/app/service/service-matrix.service.ts
+++ src/app/service/service-matrix.service.ts
@@ -13,8 +13,8 @@ export class ServiceMatrixService {
   selectedRowIndex;
   filterStore: Filters ;
   //API_URL:string = "http://ec2-18-220-6-166.us-east-2.compute.amazonaws.com:8080/";
-  API_URL:string = "http://localhost:8080/";
-  //API_URL:string = "http://cdfw-alb-prod-775994205.us-west-1.elb.amazonaws.com:9301/";
+  //API_URL:string = "http://localhost:8080/";
+  API_URL:string = "http://cdfw-alb-prod-775994205.us-west-1.elb.amazonaws.com:9301/";
    @ViewChild(MatPaginator) paginator: MatPaginator;
   inputDataStore = [{
     id: 1,
