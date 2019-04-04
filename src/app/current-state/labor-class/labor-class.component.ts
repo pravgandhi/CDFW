8ad99039b2948a9363dc567e8df12726b4fcd58c
Bura <vbura@deloitte.com>
2019-04-04 12:26:00 -0400

    Current State by Region filter

--- src/app/current-state/labor-class/labor-class.component.ts
+++ src/app/current-state/labor-class/labor-class.component.ts
@@ -18,38 +18,53 @@ export class LaborClassComponent implements OnInit {
   user: any = null;
   displayedColumns: string[] = [];
   selectedRegion: string;
+  selectedRegionObj: string;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-
+  regionList: [];
 
   constructor(private http: HttpClient, private router: Router, private serviceMatrix: ServiceMatrixService,
-    private userService:UserService, private route: ActivatedRoute) { }
+    private userService: UserService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-    this.selectedRegion = params['regionId'];
+      this.selectedRegion = params['regionId'];
       this.customInit(params);
     });
   }
 
-
-  customInit(params){
-    this.user = this.userService.user;
-    if(this.user != null && this.user["laborClassMappingsById"].length > 0){
-      this.displayedColumns = ["laborClassName", "hours"];
-      var userLsMappings = this.user["laborClassMappingsById"];
-      userLsMappings.forEach(element => {
-        this.dataSource.data.push(element["csLaborClassbyLaborClassId"]);
-      });
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
+  customInit(params) {
+    this.user = this.userService.user;    
+    this.setRegionDetails(this.user);
+    if (this.user != null && this.user["laborClassMappingsById"].length > 0) {
+      this.setDatasource(this.selectedRegion);
     }
+  }
+
+  setRegionDetails(user: Object) {
+    this.regionList = user['userRegionMappingsById'];
+  }
+
+  chooseRegion(regionId: number){
+     this.setDatasource(regionId);
+  }
+
+  setDatasource(regionId){
+    this.dataSource.data = [];
+    this.selectedRegionObj = this.regionList.find(e => e["regionId"] == regionId);
+    this.displayedColumns = ["laborClassName", "hours"];
+    var userLsMappings = this.user["laborClassMappingsById"];
+    var userLsMappingByRegion = userLsMappings.filter(e => e.regionId == regionId);
+    userLsMappingByRegion.forEach(element => {
+      this.dataSource.data.push(element["laborClass"]);
+    });
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   backToLogin() {
     this.router.navigate(["login"]);
   }
-
 
   showMatrix(row) {
     this.router.navigate(['cslaborhours']);
